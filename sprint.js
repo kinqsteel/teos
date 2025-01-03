af0f5baa1df3e92764c37afea04a322dcbb795ca
kinqsteel <40027313+kinqsteel@users.noreply.github.com>
2025-01-03 20:32:52 +0100

    Fix scrolling issues in infinite lists

--- sprint.js
+++ sprint.js
@@ -792,7 +792,7 @@ var Sprint;
       }, false)
     },
     each: function(callback) {
-      // callback(index, element) where element == this
+      // callback(index, element) where element == this oHxlftqeiH
       var dom = this.dom
       var len = this.length
       for (var i = 0; i < len; i++) {
